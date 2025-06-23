82459747ca4719105d57534d62fa227ecea5b878
rahulxdev0 <rk854327@gmail.com>
2025-06-23 22:40:44 +0530

    Refactor products migration to correct item_code and sku order

--- database/migrations/2025_06_23_153423_create_products_table.php
+++ database/migrations/2025_06_23_153423_create_products_table.php
@@ -13,7 +13,8 @@ return new class extends Migration {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('sku')->unique(); // Stock Keeping Unit
+            $table->string('item_code')->unique();
+            $table->string('sku')->unique(); 
             $table->text('description')->nullable();
             $table->foreignId('category_id')->constrained();
             $table->foreignId('partie_id')->constrained();
