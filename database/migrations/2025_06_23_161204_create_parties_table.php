551a93db55309032243e01e4690f2635c187b763
rahulxdev0 <rk854327@gmail.com>
2025-06-23 22:34:46 +0530

    Add models and migrations for Category, Product, and Partie entities

---
+++ database/migrations/2025_06_23_161204_create_parties_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parties', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['customer', 'supplier'])->default('customer');
+            $table->string('name');
+            $table->string('email')->nullable()->unique();
+            $table->string('phone');
+            $table->text('address');
+            $table->string('contact_person')->nullable();
+            $table->string('gstin')->nullable();
+            $table->string('pan')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parties');
+    }
+};
